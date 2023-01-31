fbb5b90d5a31ce43c009b5e73c86198700c3fd2d
web-js-master <web-js-master@gmail.com>
2023-01-31 20:56:06 +0300

    feat:init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
+  console.log('Server started on port: ', process.env.PORT);
 }
 bootstrap();
